577a838b33764277c747ea9afea52d2bfa108c7c
anupamamurali <anupamamurali29@gmail.com>
2021-11-12 20:28:22 +0530

    [ADD]discount_limit

--- discount_limit/models/discount.py
+++ discount_limit/models/discount.py
@@ -7,34 +7,20 @@ from dateutil.relativedelta import relativedelta
 class DiscountLimit(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    max_disc_limit = fields.Monetary(string='Discount Limit', store=True)
-
-
-   # def set_values(self):
-   #  super(DiscountLimit, self).set_values()
-   # select_type = self.env['ir.config_parameter'].sudo()
-   # select_type.set_param('discount_limit.max_disc_limit', self.max_disc_limit)
-
-   # @api.model
-   # def get_values(self):
-   #     res = super(DiscountLimit, self).get_values()
-   #    select_type = self.env['ir.config_parameter'].sudo()
-   #   sell = select_type.get_param('discount_limit.max_disc_limit')
-   #  res.update({'max_disc_limit': sell})
-   # return res
+    max_disc_limit = fields.Monetary(string='Discount Limit')
 
     def set_values(self):
         super(DiscountLimit, self).set_values()
-        set_param = self.env['ir.config_parameter'].sudo().set_param
-        set_param('discount_limit.max_disc_limit', float(self.max_disc_limit))
-        print(set_param)
+        select_type = self.env['ir.config_parameter'].sudo()
+        select_type.set_param('discount_limit.max_disc_limit',
+                              self.max_disc_limit)
 
     @api.model
     def get_values(self):
         res = super(DiscountLimit, self).get_values()
-        get_param = self.env['ir.config_parameter'].sudo().get_param
-        res['max_disc_limit'] = float(get_param('discount_limit.max_disc_limit'))
-        print(res)
+        select_type = self.env['ir.config_parameter'].sudo()
+        update_val = select_type.get_param('discount_limit.max_disc_limit')
+        res.update({'max_disc_limit': update_val})
         return res
 
 
@@ -43,8 +29,8 @@ class SaleOrderInherit(models.Model):
 
     def action_confirm(self):
         records = self.env['sale.order'].search([('date_order',
-                '<=', (datetime.today() + relativedelta(day=31)).strftime('%Y-%m-%d')),
-                ('date_order', '>=', (datetime.today()-relativedelta(day=1)).strftime('%Y-%m-%d'))])
+                  '<=', (datetime.today() + relativedelta(day=31)).strftime('%Y-%m-%d')),
+                     ('date_order', '>=', (datetime.today()-relativedelta(day=1)).strftime('%Y-%m-%d'))])
         total = 0
         for rec in records:
             for line in rec.order_line:
@@ -54,9 +40,9 @@ class SaleOrderInherit(models.Model):
                 discount = total_price - sub_total
                 total += discount
         print(total)
-        limit = self.env['ir.config_parameter'].sudo().get_param('max_disc_limit')
+        limit = self.env['ir.config_parameter'].sudo().get_param('discount_limit.max_disc_limit') or False
         print(limit)
-        if total > limit:
+        if total > float(limit):
             raise UserError("Warning...! Discount exceed the limit")
         result = super(SaleOrderInherit, self).action_confirm()
         return result
