5f00764165df6cfad1e99a0cd9b3913643c8ce25
anupamamurali <anupamamurali29@gmail.com>
2021-11-19 21:40:55 +0530

    [ADD]discount_limit

--- discount_limit/models/discount.py
+++ discount_limit/models/discount.py
@@ -2,12 +2,14 @@ from odoo import api, fields, models
 from odoo.exceptions import UserError
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import calendar
 
 
 class DiscountLimit(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    max_disc_limit = fields.Monetary(string='Discount Limit')
+    monthly_discount = fields.Boolean(string='Monthly Discount Limit', config_parameter='discount_limit.monthly_discount')
+    max_disc_limit = fields.Monetary(string='Maximum Discount')
 
     def set_values(self):
         super(DiscountLimit, self).set_values()
@@ -28,8 +30,11 @@ class SaleOrderInherit(models.Model):
     _inherit = 'sale.order'
 
     def action_confirm(self):
+        year = self.date_order.year
+        month = self.date_order.month
+        days_count = (calendar.monthrange(year, month))[1]
         records = self.env['sale.order'].search([('date_order',
-                  '<=', (datetime.today() + relativedelta(day=31)).strftime('%Y-%m-%d')),
+                  '<=', (datetime.today() + relativedelta(day=days_count)).strftime('%Y-%m-%d')),
                      ('date_order', '>=', (datetime.today()-relativedelta(day=1)).strftime('%Y-%m-%d'))])
         total = 0
         for rec in records:
@@ -39,9 +44,7 @@ class SaleOrderInherit(models.Model):
                 sub_total = line.price_subtotal
                 discount = total_price - sub_total
                 total += discount
-        print(total)
         limit = self.env['ir.config_parameter'].sudo().get_param('discount_limit.max_disc_limit') or False
-        print(limit)
         if total > float(limit):
             raise UserError("Warning...! Discount exceed the limit")
         result = super(SaleOrderInherit, self).action_confirm()
