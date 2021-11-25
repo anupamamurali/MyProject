22f82d7371a59138e5f12ef34933c46e30d0e40c
anupamamurali <anupamamurali29@gmail.com>
2021-11-25 21:47:30 +0530

    [ADD]pos_avail_quant

---
+++ pos_avail_quant/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Product Available Quantity',
+    'version': '14.0.1.0.0',
+    'summary': 'Product Available Quantity',
+    'sequence': -100,
+    'description': """Product Available Quantity In POS""",
+    'category': 'Hidden',
+    'website': '',
+    'depends': [
+        'point_of_sale',
+        'product',
+        'stock'
+    ],
+    'data': [
+        'views/pos_location.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
