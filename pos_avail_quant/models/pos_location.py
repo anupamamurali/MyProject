bd2f8934c435f81d9d05f1157742d2d4c314fb0c
anupamamurali <anupamamurali29@gmail.com>
2021-11-26 07:53:13 +0530

    [ADD]pos_avail_quant

--- pos_avail_quant/models/pos_location.py
+++ pos_avail_quant/models/pos_location.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class PosLocation(models.Model):
     _inherit = 'pos.config'
 
-    invent_loc = fields.Many2one('stock.location', string='Location', help='To select the location.')
+    invent_loc_id = fields.Many2one('stock.location', string='Location', help='To select the location.')
