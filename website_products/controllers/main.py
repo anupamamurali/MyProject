a09ae99467ba6bead3a0fae2d7d81aa83103f8fa
anupamamurali <anupamamurali29@gmail.com>
2021-12-02 22:14:55 +0530

    [ADD]website_products

--- website_products/controllers/main.py
+++ website_products/controllers/main.py
@@ -12,22 +12,43 @@ class WebsiteSaleInherit(WebsiteSale):
         '''/shop/category/<model("product.public.category"):category>/page/<int:page>'''
     ], type='http', auth="public", website=True)
     def shop(self, page=0, category=None, search='', ppg=False, **post):
+        res = super(WebsiteSaleInherit, self).shop(page=0, category=None,
+                                                   search='', ppg=False,
+                                                   **post, )
         print("Inherited Odoo")
-        user_id = request.uid + 1
-        print(user_id)
-        user_rec = request.env['res.partner'].search([('id', '=', user_id)])
-        category_id = []
-        for rec in user_rec:
-            category_id += rec.pro_cat_ids
-        print(category_id)
-        products = user_rec.product_ids
-        Category = request.env['product.public.category'].search([('id', '=', category_id)])
-       # category = Category.search([('id', 'in', category_id())])
+        user_id = request.env.user
+        Category = request.env['product.public.category']
+        Product = request.env['product.template'].with_context(bin_size=True)
         print(Category)
-       # print(category)
-        # values = {
-        #     'category': categories,
-        #     'products': products
-        # }
-        res = super(WebsiteSaleInherit, self).shop(page=0, category=None, search='', ppg=False, **post)
+        print(Product)
+        print(user_id.partner_id)
+        user_rec = request.env['res.partner'].search([('id', '=', user_id.partner_id.id)])
+        values = {}
+        category_id = []
+        product_id = []
+        if user_rec.pro_cat_ids:
+            for rec in user_rec.pro_cat_ids:
+                category_id.append(rec.id)
+            category = Category.search([('id', 'in', category_id)])
+            print(category)
+        else:
+            category = Category
+        values.update({'category': category})
+        print("values=", values)
+        if user_rec.product_ids:
+            for rec in user_rec.product_ids:
+                product_id.append(rec.id)
+            product = Product.search([('id', 'in', product_id)])
+            print(product)
+        else:
+            product = Product
+        values.update({'category': category, 'product': product})
+        print("values=", values)
+        if category:
+            values['main_object'] = category, product
+        print(values)
+        var = res.qcontext.get('products')
+        var = product
+        print("var=", var)
+        print(res.qcontext.update({'products': product}))
         return res
